87b74a975abe587b1b486dde542a34d2ddaef8ae
Yanickd10 <yanickdushime2002@gmail.com>
2025-04-30 18:02:09 +0200

    Auto-commit Wed 04/30/2025 18:02:09.91

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 <?php include("includes/main-page.php");?> 
 <?php include("includes/footer.php");?>  
 <?php include("includes/back-to-top.php");?> 
-<script src="assets/js/nav.js"></script>
+<script src="assets/js/nav.js"> </script>
 <script src="assets/js/smooth-scroll.js"></script>
 </body>
 </html> 
